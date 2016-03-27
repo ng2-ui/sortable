76ffe60284dd8af3e022949d578cdbadd030adfb
Allen Kim <allenhwkim@gmail.com>
2016-03-27 19:12:45 -0400

    updated

--- make.js
+++ make.js
@@ -1,17 +1,10 @@
-var pkg     = require('./package.json');
 var path    = require('path');
 var Builder = require('systemjs-builder');
-var name    = pkg.name;
 
 var builder = new Builder();
-var config = {
+builder.config({
   baseURL: '.',
-  transpiler: 'typescript',
-  typescriptOptions: {
-    module: 'cjs'
-  },
   map: {
-    typescript: './node_modules/typescript/lib/typescript.js',
     angular2: path.resolve('node_modules/angular2'),
     rxjs: path.resolve('node_modules/rxjs')
   },
@@ -22,15 +15,6 @@ var config = {
     'node_modules/angular2/*': { build: false },
     'node_modules/rxjs/*': { build: false }
   },
-};
-
-builder.config(config);
-
-builder
-.bundle(name, path.resolve(__dirname, 'bundles/', name + '.js'))
-.then(function() {
-  console.log('Build complete.');
-})
-.catch(function(err) {
-  console.log('Error', err);
 });
+
+builder.bundle('ng2-package-seed', 'bundles/ng2-package-seed.js');
