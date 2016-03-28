997ef64516b87c20e90ebdeb7dd216f00dd15150
Allen Kim <allenhwkim@gmail.com>
2016-03-27 21:44:56 -0400

    updated

--- make.js
+++ make.js
@@ -1,20 +1,32 @@
+var pkg     = require('./package.json');
 var path    = require('path');
 var Builder = require('systemjs-builder');
+var name    = pkg.name;
 
 var builder = new Builder();
-builder.config({
+var config = {
   baseURL: '.',
+  transpiler: 'typescript',
+  typescriptOptions: {
+    module: 'commonjs'
+  },
   map: {
+    typescript: './node_modules/typescript/lib/typescript.js',
     angular2: path.resolve('node_modules/angular2'),
     rxjs: path.resolve('node_modules/rxjs')
   },
   paths: {
-    '*': '*.js'
+    '*': '*.ts'
   },
   meta: {
     'node_modules/angular2/*': { build: false },
     'node_modules/rxjs/*': { build: false }
   },
-});
+};
 
-builder.bundle('ng2-package-seed', 'bundles/ng2-package-seed.js');
+builder.config(config);
+
+builder.bundle(
+  'ng2-package-seed', 
+  'dist/ng2-package-seed.js', {minify: true, sourceMaps: true}
+);
