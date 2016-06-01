bdd4291b1f82744c7381f6c0db83d105409718ac
Allen Kim <allenhwkim@gmail.com>
2016-05-31 22:48:02 -0400

    added *.d.ts for distribution

---
+++ dist/app/hello-world.d.ts
@@ -0,0 +1,3 @@
+export declare class HelloWorld {
+    helloWorld(): void;
+}
---
+++ dist/app/hello-world.js
@@ -0,0 +1,11 @@
+"use strict";
+var HelloWorld = (function () {
+    function HelloWorld() {
+    }
+    HelloWorld.prototype.helloWorld = function () {
+        console.log('Hello World');
+    };
+    return HelloWorld;
+}());
+exports.HelloWorld = HelloWorld;
+//# sourceMappingURL=hello-world.js.map
