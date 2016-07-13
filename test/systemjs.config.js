401f502b9f5522b258dee8230c71e0ac828bd042
Allen Kim <allenhwkim@gmail.com>
2016-07-13 00:59:42 -0400

    updated

--- test/systemjs.config.js
+++ test/systemjs.config.js
@@ -7,18 +7,19 @@
   var packages = {
     app: { main: './main.ts', defaultExtension: 'ts' },
     '@angular/core': { main: 'bundles/core.umd.js', defaultExtension: 'js' },
+    '@angular/http': { main: 'bundles/http.umd.js', defaultExtension: 'js' },
     '@angular/compiler': { main: 'bundles/compiler.umd.js', defaultExtension: 'js' },
     '@angular/common': { main: 'bundles/common.umd.js', defaultExtension: 'js' },
     '@angular/platform-browser-dynamic': { main: 'bundles/platform-browser-dynamic.umd.js', defaultExtension: 'js' },
     '@angular/platform-browser': { main: 'bundles/platform-browser.umd.js', defaultExtension: 'js' },
     rxjs: { defaultExtension: 'js' }
   };
-  
-  map['my-npm'] = '../src';
-  packages['my-npm'] = {main: 'index.ts', defaultExtension: 'ts'};
+
   map['my-npm'] = '../dist';
   packages['my-npm'] = {main: 'index.js', defaultExtension: 'js'};
-  
+  map['my-npm'] = '../src';
+  packages['my-npm'] = {main: 'index.ts', defaultExtension: 'ts'};
+
   System.config({
     transpiler: 'typescript', //use typescript for compilation
     typescriptOptions: {      //typescript compiler options
